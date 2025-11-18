6d2f09f3afcea073823ae011bb4ed9d2a18013a3
Mikhail Prikhodko <prihodko@zm.team>
2025-11-18 22:33:26 +0900

    7.3 Валидация JSON schema

---
+++ json_schema_example.py
@@ -0,0 +1,21 @@
+from jsonschema import validate, ValidationError
+
+schema = {
+  "type": "object",
+  "properties": {
+    "name": { "type": "string" },
+    "age": { "type": "number" }
+  },
+  "required": ["name"]
+}
+
+data = {
+  "name": "John Doe",
+  "age": 30
+}
+
+try:
+    validate(instance=data, schema=schema)
+    print("Данные соответствуют схеме.")
+except ValidationError as e:
+    print(f"Ошибка валидации: {e.message}")
