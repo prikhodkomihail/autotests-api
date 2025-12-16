a1b4ebc46d5993b3d0d051c2a376812f5f3f3527
Mikhail Prikhodko <prihodko@zm.team>
2025-12-16 21:45:50 +0300

    9.2 Практикуемся в написании API автотестов. Часть 1. Практика написания негативных API автотестов для работы с файлами

--- tests/files/test_files.py
+++ tests/files/test_files.py
@@ -11,7 +11,8 @@ from fixtures.files import FileFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.files import (
     assert_create_file_with_empty_directory_response,
-    assert_create_file_with_empty_filename_response)
+    assert_create_file_with_empty_filename_response,
+    assert_get_file_with_incorrect_file_id_response)
 from tools.assertions.files import (assert_create_file_response, assert_file_not_found_response,
                                     assert_get_file_response)
 from tools.assertions.schema import validate_json_schema
@@ -66,7 +67,7 @@ class TestFiles:
 
         validate_json_schema(response.json(), response_data.model_json_schema())
 
-    
+
     def test_delete_file(self, files_client: FilesClient, function_file: FileFixture):
         delete_response = files_client.delete_file_api(function_file.response.file.id)
 
@@ -79,3 +80,14 @@ class TestFiles:
         assert_file_not_found_response(get_response_data)
         
         validate_json_schema(get_response.json(), get_response_data.model_json_schema())
+
+
+    def test_get_file_with_incorrect_file_id(self, files_client: FilesClient):
+        response = files_client.get_file_api("incorrect-file-id")
+        response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
+        assert_get_file_with_incorrect_file_id_response(response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+        
--- tools/assertions/files.py
+++ tools/assertions/files.py
@@ -91,6 +91,7 @@ def assert_create_file_with_empty_directory_response(actual: ValidationErrorResp
             )
         ]
     )
+    assert_validation_error_response(actual, expected)
 
 
 def assert_file_not_found_response(actual: InternalErrorResponseSchema):
@@ -102,3 +103,31 @@ def assert_file_not_found_response(actual: InternalErrorResponseSchema):
     """
     expected = InternalErrorResponseSchema(details="File not found") # type: ignore
     assert_internal_error_response(actual, expected)
+
+
+def assert_get_file_with_incorrect_file_id_response(actual: ValidationErrorResponseSchema):
+    """
+    Проверяет, что ответ на получение файла с некорректным file_id
+    соответствует ожидаемой валидационной ошибке.
+
+    :param actual: Ответ от API с ошибкой валидации, который необходимо проверить.
+    :raises AssertionError: Если фактический ответ не соответствует ожидаемому.
+    """
+    expected = ValidationErrorResponseSchema(
+        details=[ # type: ignore
+            ValidationErrorSchema(
+                type="uuid_parsing",
+                input="incorrect-file-id",
+                context={  # type: ignore
+                    "error": "invalid character: expected an optional prefix of `urn:uuid:` "
+                             "followed by [0-9a-fA-F-], found `i` at 1"
+                },
+                message=# type: ignore
+                    "Input should be a valid UUID, invalid character: "
+                    "expected an optional prefix of `urn:uuid:` followed by "
+                    "[0-9a-fA-F-], found `i` at 1",
+                location=["path", "file_id"]  # type: ignore
+            )
+        ]
+    )
+    assert_validation_error_response(actual, expected)
