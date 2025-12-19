a4f905bb43f6cfe0bd61a3f3cdeb0507c8d579c2
Mikhail Prikhodko <prihodko@zm.team>
2025-12-19 09:17:43 +0300

    9.4 Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Обновление задания

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,4 +1,4 @@
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, ExerciseSchema, GetExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, ExerciseSchema, GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -52,4 +52,22 @@ def assert_get_exercise_response(
     :raises AssertionError: Если данные упражнения не совпадают.
     """
     assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
-    
+
+
+def assert_update_exercise_response(
+        response: UpdateExerciseResponseSchema, 
+        request: UpdateExerciseRequestSchema
+):
+    """
+    Проверяет, что ответ на обновление упражнения соответствует данным из запроса.
+
+    :param request: Исходный запрос на обновление упражнения.
+    :param response: Ответ API с обновленными данными упражнения.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(response.exercise.title, request.title, "title")
+    assert_equal(response.exercise.max_score, request.max_score, "max_score")
+    assert_equal(response.exercise.min_score, request.min_score, "min_score")
+    assert_equal(response.exercise.order_index, request.order_index, "order_index")
+    assert_equal(response.exercise.description, request.description, "description")
+    assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
