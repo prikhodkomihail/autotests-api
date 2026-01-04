5fb29386b642418ce54ba5bd772a1557954131c1
Mikhail Prikhodko <prihodko@zm.team>
2026-01-04 15:49:47 +0300

    11.1 Настройки автотестов с Pydantic. Практика работы с настройками

--- api_client_create_course.py
+++ api_client_create_course.py
@@ -1,11 +1,11 @@
 from clients.courses.courses_client import get_courses_client
+from clients.courses.courses_schema import CreateCourseRequestSchema
 from clients.files.files_client import get_files_client
 from clients.files.files_schema import CreateFileRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
-from tools.fakers import fake
-from clients.courses.courses_schema import CreateCourseRequestSchema
+from config import settings
 
 public_users_client = get_public_users_client()
 
@@ -19,7 +19,7 @@ authentication_user = AuthenticationUserSchema(
 files_client = get_files_client(authentication_user)
 courses_client = get_courses_client(authentication_user)
 
-create_file_request = CreateFileRequestSchema(upload_file='./testdata/files/image.png')
+create_file_request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
 create_file_response = files_client.create_file(create_file_request)
 print(f'Create file data: {create_file_response}')
 
--- fixtures/files.py
+++ fixtures/files.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 
 from clients.files.files_schema import CreateFileRequestSchema, CreateFileResponseSchema
 from fixtures.users import UserFixture
+from config import settings
 
 
 class FileFixture(BaseModel):
@@ -18,6 +19,6 @@ def files_client(function_user: UserFixture) -> FilesClient:
 
 @pytest.fixture
 def function_file(files_client: FilesClient) -> FileFixture:
-    request = CreateFileRequestSchema(upload_file='./testdata/files/image.png')
+    request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
     response = files_client.create_file(request)
     return FileFixture(request=request, response=response)
